7e8c48aebceece06b63b89c5cc6c1d324952e5bd
Dawei Li <thomashyde23@gmail.com>
2023-06-07 22:11:00 -0700

    first commit - docker setup + pg DB setup @ 2023-06-07 22:10 PDT

---
+++ docker_sql/pipeline.py
@@ -0,0 +1,7 @@
+import sys
+import pandas as pd
+from datetime import datetime, timedelta
+
+date=str(datetime.now())[:10]
+
+print(f'Data pipeline finished successfully for {date}')
---
+++ docker_sql/run_db.sh
@@ -0,0 +1,1 @@
+pgcli -h localhost -p 5432 -u root -d ny_taxi
